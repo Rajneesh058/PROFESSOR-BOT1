46cdb4dd72ae6c00e635f5ed46a62aa00436b6e2
Rajneesh058 <114851821+Rajneesh058@users.noreply.github.com>
2022-12-04 18:48:57 +0530

    Add files via upload

--- plugins/ExraMods/carbon.py
+++ plugins/ExraMods/carbon.py
@@ -42,8 +42,8 @@ async def carbon_func(_, message):
     await m.edit("ᴜᴘʟᴏᴀᴅɪɴɢ..")
     await message.reply_photo(
         photo=carbon,
-        caption="**Made by @mkn_bots_updates**",
-        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("𝚂𝚄𝙿𝙿𝙾𝚁𝚃 𝚄𝚂", url="https://t.me/mkn_bots_updates")]]),                   
+        caption="**Made by @Movie_Megaverse_Official**",
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🎭ᴍᴏᴠɪᴇ ᴄʜᴀɴɴᴇʟ🎭", url="t.me/Movie_Megaverse_official")]]),                   
     )
     await m.delete()
     carbon.close()
--- plugins/ExraMods/lyrics.py
+++ plugins/ExraMods/lyrics.py
@@ -20,9 +20,9 @@ async def sng(bot, message):
           await mee.delete()
           try:
             await mee.delete()
-            await bot.send_message(chat_id, text = rpl, reply_to_message_id = message.id, reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇs ", url = f"t.me/mkn_bots_updates")]]))
+            await bot.send_message(chat_id, text = rpl, reply_to_message_id = message.id, reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🎭ᴍᴏᴠɪᴇ ᴜᴘᴅᴀᴛᴇs🎭", url = f"t.me/Movie_Megaverse_official")]]))
           except Exception as e:                            
-             await message.reply_text(f"I Can't Find A Song With `{song}`", quote = True, reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇs", url = f"t.me/mkn_bots_updates")]]))
+             await message.reply_text(f"I Can't Find A Song With `{song}`", quote = True, reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🎭ᴍᴏᴠɪᴇ ᴜᴘᴅᴀᴛᴇs🎭", url = f"t.me/Movie_Megaverse_official")]]))
 
 
 def search(song):
