c49745f84d8aa501d8912e3c20f28ea7c10b9167
Rajneesh058 <114851821+Rajneesh058@users.noreply.github.com>
2023-03-02 10:58:36 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -7,6 +7,6 @@ async def root_route_handler(request):
     return web.json_response("Megaverse Bot")
 
 async def web_server():
-    web_app = web.Application(client_max_size=3000)
+    web_app = web.Application(client_max_size=300)
     web_app.add_routes(routes)
     return web_app
